ba282a02772a9180bd81acc97eff2cdc6d1a48dc
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2025-10-21 22:59:40 +0200

    ✨ Add splitter #2

--- start-pipeline.sh
+++ start-pipeline.sh
@@ -6,6 +6,7 @@ docker-compose -p ial-pipeline \
   -f box/broker-client/docker-compose.yml \
   -f pipeline/cleaner/docker-compose.yml \
   -f pipeline/normalizer/docker-compose.yml \
+  -f pipeline/splitter/docker-compose.yml \
   up -d
 
 echo "✅ Pipeline started successfully"
